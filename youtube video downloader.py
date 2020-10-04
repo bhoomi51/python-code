ac9f4fdd005e8ba0d25cdcabc0575e1ea7ce2a77
bhoomi51 <72351205+bhoomi51@users.noreply.github.com>
2020-10-05 00:24:56 +0530

    Add files via upload

---
+++ youtube video downloader.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+
+# where to save
+SAVE_PATH = "E:/"  
+
+# link of the video to be downloaded
+link = ["https://www.youtube.com/watch?v=yVQGM4jKtos"]
+for i in link:
+    try:
+        # object creation using YouTube which was imported in the beginning
+        yt = YouTube(i)
+    except:
+        print("Connection Error")  # to handle exception
+
+    # filters out all the files with "mp4" extension
+    mp4files = yt.filter('mp4')
+
+    # get the video with the extension and resolution passed in the get() function
+    d_video = yt.get(mp4files[-1].extension, mp4files[-1].resolution)
+    try:
+        # downloading the video
+        d_video.download(SAVE_PATH)
+    except:
+        print("Some Error!")
+print('Task Completed!')
